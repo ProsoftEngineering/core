077fdf246fd394c1413692b56f474d8e5b23b5e9
Brian Bergstrand <bbergstrand@prosofteng.com>
2015-12-09 23:47:31 -0600

    Update get_cmake to take an install root arg and use a default version if one is not provided

--- depend/get_cmake.sh
+++ depend/get_cmake.sh
@@ -2,7 +2,7 @@
 #
 # Copyright © 2015, Prosoft Engineering, Inc. (A.K.A "Prosoft")
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #     * Redistributions of source code must retain the above copyright
@@ -13,7 +13,7 @@
 #     * Neither the name of Prosoft nor the names of its contributors may be
 #       used to endorse or promote products derived from this software without
 #       specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -27,13 +27,30 @@
 
 set -e
 
-# Could not find a link/feed to automatically find the latest version, so we have to ask.
-CMAKE_VER=0
-if [ ! -z $1 ]; then
-CMAKE_VER=$1
-else
-echo "What version?"
-read CMAKE_VER
+while : ; do
+	case $# in 0) break ;; esac
+    
+	case "$1" in
+		--prefix )
+			shift
+			INSTALL_DIR="$1"
+			shift
+		;;
+		--cmake )
+			shift
+			CMAKE_VER="$1"
+			shift
+		;;
+		* )
+		    echo "Unknown argument: ${1}"
+		    break
+		;;
+	esac
+done
+
+# Could not find a link/feed to automatically find the latest version, so we just hardcode it.
+if [ -z $CMAKE_VER ]; then
+CMAKE_VER=3.4.1
 fi
 
 VROOT=`echo ${CMAKE_VER} | grep -oE '[0-9]\.[0-9]'`
@@ -57,7 +74,12 @@ BUILD_DIR=cmake-tmp-$$
 mkdir ${BUILD_DIR}
 cd ${BUILD_DIR}
 tar -xzf ../cmake-${CMAKE_VER}.tar.gz --strip-components 1
+
+if [ -z $INSTALL_DIR ]; then
 ./bootstrap && make && sudo make install
+else
+./bootstrap --prefix="${INSTALL_DIR}" && make && make install
+fi
 
 if [ $? -eq 0 ]; then
 cd ..
