e2755deb13e5cb2bf2efcfbbc29f596fff25bbbd
Brian Bergstrand <bbergstrand@prosofteng.com>
2016-03-22 14:37:56 -0500

    Move rake utils

--- prosoft/core/tests/rakefile.rb
+++ prosoft/core/tests/rakefile.rb
@@ -23,7 +23,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-require_relative 'rakefile_utils.rb'
+require_relative '../rake/utils.rb'
 
 task :default => [:all]
 
