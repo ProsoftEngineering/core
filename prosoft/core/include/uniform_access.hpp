f1ac8d437214f75b3a708f125e17d12cea50ef3f
Brian Bergstrand <bbergstrand@prosofteng.com>
2017-09-03 10:04:06 -0500

    Always use extendend-constexpr version for any compiler that supports it
    The loop will likely be significantly faster than recursion at runtime.

--- prosoft/core/include/uniform_access.hpp
+++ prosoft/core/include/uniform_access.hpp
@@ -67,7 +67,7 @@ struct access_traits<T*, typename std::enable_if<std::is_integral<T>::value>::ty
     using const_value_pointer = const T*;
     
     constexpr size_t data_size(const_value_pointer t) const {
-#if _MSC_VER == 1910
+#if PS_COMPLETE_CPP14
 // Bug in VS 2017.0: https://developercommunity.visualstudio.com/content/problem/10720/constexpr-function-accessing-character-array-leads.html
 // Requires C++14 expanded constexpr.
         int i = 0;
