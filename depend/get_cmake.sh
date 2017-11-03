169a5546f76dd982dcebea5673aa88733d0143ac
Brian Bergstrand <bbergstrand@prosofteng.com>
2017-11-03 11:51:28 -0500

    Don't disable SSL download for macOS

--- depend/get_cmake.sh
+++ depend/get_cmake.sh
@@ -58,7 +58,9 @@ VROOT=`echo ${CMAKE_VER} | grep -oE '[0-9]\.[0-9]'`
 URL=https://cmake.org/files/v${VROOT}/cmake-${CMAKE_VER}.tar.gz
 
 cd ${TMPDIR}
-if [ `uname -s` != 'Linux' ]; then
+if [ `uname -s` == 'Darwin' ]; then
+curl -O ${URL}
+elif [ `uname -s` != 'Linux' ]; then
 # Note: recent versions of CuRL ship with TLS basically disabled since they do not provide a root cert trust.
 # See:
 # http://curl.haxx.se/docs/sslcerts.html
