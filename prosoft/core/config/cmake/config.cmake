ece9453f4f46e7c0a2807e772572f2384ced379c
Brian Bergstrand <bbergstrand@prosofteng.com>
2018-03-10 11:22:24 -0600

    Add stable build type identifiers

--- prosoft/core/config/cmake/config.cmake
+++ prosoft/core/config/cmake/config.cmake
@@ -1,4 +1,4 @@
-# Copyright © 2013-2015, Prosoft Engineering, Inc. (A.K.A "Prosoft")
+# Copyright © 2013-2018, Prosoft Engineering, Inc. (A.K.A "Prosoft")
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -32,14 +32,22 @@ if(NOT CMAKE_BUILD_TYPE)
     message(FATAL_ERROR, "CMake build type not set!")
 endif()
 
-if(NOT DEFINED DEBUG)
+# PS_BUILD_* are stable build type identifiers. They should never be set anywhere but here.
+# This is not true of 'DEBUG'.
+if(NOT DEFINED PS_BUILD_DEBUG)
     # CMake doesn't have a case-insensitive string compare
     string(TOLOWER "${CMAKE_BUILD_TYPE}" CMAKE_BUILD_TYPE_TOLOWER)
     if(CMAKE_BUILD_TYPE_TOLOWER STREQUAL "release" OR CMAKE_BUILD_TYPE_TOLOWER STREQUAL "relwithdebinfo")
-        set(DEBUG false)
+        set(PS_BUILD_DEBUG false)
+        set(PS_BUILD_RELEASE true)
     else()
-        set(DEBUG true)
+        set(PS_BUILD_DEBUG true)
+        set(PS_BUILD_RELEASE false)
     endif()
+endif()
+
+if(NOT DEFINED DEBUG)
+    set(DEBUG ${PS_BUILD_DEBUG})
 endif()
 
 include("${PSCONFIG}/cmake/config_compiler.cmake")
--- prosoft/core/core.cmake
+++ prosoft/core/core.cmake
@@ -1,4 +1,4 @@
-# Copyright © 2015-2017, Prosoft Engineering, Inc. (A.K.A "Prosoft")
+# Copyright © 2015-2018, Prosoft Engineering, Inc. (A.K.A "Prosoft")
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -38,8 +38,13 @@ endmacro()
 
 macro(ps_core_configure_required TARGET_NAME)
     ps_core_include(${TARGET_NAME})
-    if(DEBUG)
-        target_compile_definitions(${TARGET_NAME} PRIVATE DEBUG=1)
+    # PS_BUILD_* are stable build type defines. They should never be set anywhere but here.
+    # Unlike 'DEBUG' which can be set externally and even '=1' for release.
+    if(PS_BUILD_DEBUG)
+        target_compile_definitions(${TARGET_NAME} PRIVATE DEBUG=1 PS_BUILD_DEBUG=1 PS_BUILD_RELEASE=0)
+    else()
+        # For legacy reasons (#ifdef instead of #if) DEBUG should not be defined in release builds.
+        target_compile_definitions(${TARGET_NAME} PRIVATE PS_BUILD_DEBUG=0 PS_BUILD_RELEASE=1)
     endif()
     if(WIN32 AND BUILD_SHARED_LIBS)
         get_target_property(TGTYPE ${TARGET_NAME} TYPE)
