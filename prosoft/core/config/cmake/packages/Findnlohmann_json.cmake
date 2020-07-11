cb373cb01efd1c1bfaabe5d80b7aea466f6b31c7
Michael Filippov <mfilippov@prosofteng.com>
2020-07-11 17:52:25 +0700

    cmake: upgrade nlohmann/json (v3.7.3 -> v3.8.0)

--- prosoft/core/config/cmake/packages/Findnlohmann_json.cmake
+++ prosoft/core/config/cmake/packages/Findnlohmann_json.cmake
@@ -1,9 +1,9 @@
 if(NOT TARGET nlohmann_json::nlohmann_json)
     include(FetchContent)
     FetchContent_Declare(nlohmann_json
-        # smaller than json.hpp
-        URL "https://github.com/nlohmann/json/releases/download/v3.7.3/include.zip"
-        URL_HASH "MD5=fb96f95cdf609143e998db401ca4f324"
+        # include.zip is smaller than json.hpp
+        URL "https://github.com/nlohmann/json/releases/download/v3.8.0/include.zip"
+        URL_HASH "MD5=ccd5d58f7344e7a53663a381030ddcd5"
         PATCH_COMMAND "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_LIST_DIR}/nlohmann_json/patch.cmake"
     )
     FetchContent_MakeAvailable(nlohmann_json)
