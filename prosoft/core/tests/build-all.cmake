386e939fb52f38280825435c991406a33385c3bb
Michael Filippov <mfilippov@prosofteng.com>
2024-03-23 05:16:18 -0700

    build-all.cmake: implement BUILDER MSVC code

--- prosoft/core/tests/build-all.cmake
+++ prosoft/core/tests/build-all.cmake
@@ -36,7 +36,31 @@ foreach(BUILDER IN LISTS BUILDERS_LIST)
         run_build_cmake(build_Makefiles_Debug
                         "-DGENERATOR=Unix Makefiles" -DBUILD_TYPE=Debug)
     elseif(BUILDER STREQUAL "MSVC")
-        message("MSVC")
+        if(EXISTS "C:/Program Files/Microsoft Visual Studio/2022")
+            set(GENERATOR "Visual Studio 17 2022")
+            set(GENERATORx86 ${GENERATOR})
+            set(ARGSx86 -DPLATFORM=Win32 -DARCH=x86)
+        elseif(EXISTS "C:/Program Files (x86)/Microsoft Visual Studio/2019")
+            set(GENERATOR "Visual Studio 16 2019")
+            set(GENERATORx86 ${GENERATOR})
+            set(ARGSx86 -DPLATFORM=Win32 -DARCH=x86)
+        elseif(EXISTS "C:/Program Files (x86)/Microsoft Visual Studio/2017")
+            set(GENERATOR "Visual Studio 15 2017 Win64")
+            set(GENERATORx86 "Visual Studio 15 2017")
+            set(ARGSx86 -DARCH=x86)
+        else()
+            message(FATAL_ERROR "Unknown MSVC version")
+        endif()
+        run_build_cmake(build_MSVC_RelWithDebInfo
+                        "-DGENERATOR=${GENERATOR}" -DBUILD_TYPE=RelWithDebInfo)
+        run_build_cmake(build_MSVC_Debug
+                        "-DGENERATOR=${GENERATOR}" -DBUILD_TYPE=Debug)
+        run_build_cmake(build_MSVCx86_RelWithDebInfo
+                        "-DGENERATOR=${GENERATORx86}" -DBUILD_TYPE=RelWithDebInfo
+                        ${ARGSx86})
+        run_build_cmake(build_MSVCx86_Debug
+                        "-DGENERATOR=${GENERATORx86}" -DBUILD_TYPE=Debug
+                        ${ARGSx86})
     elseif(BUILDER STREQUAL "NINJA")
         run_build_cmake(build_Ninja_RelWithDebInfo
                         -DGENERATOR=Ninja -DBUILD_TYPE=RelWithDebInfo)
