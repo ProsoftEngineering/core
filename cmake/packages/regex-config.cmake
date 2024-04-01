9c6cb734bc4167829f746c0bf720ac5385973512
Michael Filippov <mfilippov@prosofteng.com>
2024-04-01 18:09:24 +0700

    Unify prefix in all modules ("ps_..." and "ps::...")

--- cmake/packages/regex-config.cmake
+++ cmake/packages/regex-config.cmake
@@ -1,5 +1,5 @@
-if(NOT TARGET regex)
+if(NOT TARGET ps_regex)
     add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../../prosoft/core/modules/regex
-                     ${CMAKE_BINARY_DIR}/regex
+                     ${CMAKE_BINARY_DIR}/ps_regex
     )
 endif()
