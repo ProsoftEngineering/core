dae40741787c488f04dcb18ff3d4e13cc8a7f364
Michael Filippov <mfilippov@prosofteng.com>
2024-05-06 04:10:25 -0700

    filesystem/change_iterator: extract change_iterator_internal.hpp for testing

--- modules/filesystem/src/change_iterator.cpp
+++ modules/filesystem/src/change_iterator.cpp
@@ -23,119 +23,16 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <prosoft/core/config/config_platform.h>
-
-#include <atomic>
-#include <thread>
-#include <unordered_set>
-
-#include <prosoft/core/include/stable_hash_wrapper.hpp>
-
-#include <prosoft/core/modules/filesystem/filesystem.hpp>
-#include "filesystem_private.hpp"
-#include <prosoft/core/modules/filesystem/filesystem_change_monitor.hpp>
+#include <prosoft/core/modules/filesystem/filesystem.hpp>   // PS_HAVE_FILESYSTEM_CHANGE_ITERATOR
 
 #if PS_HAVE_FILESYSTEM_CHANGE_ITERATOR
 
-namespace fs = prosoft::filesystem::v1;
+#include "change_iterator_internal.hpp"
+#include "filesystem_private.hpp"
 
-namespace {
-
-using extraction_type = std::vector<fs::path>;
-
-fs::change_event to_events(fs::directory_options opts) {
-    using namespace fs;
-    fs::change_event ev = change_event::rescan_required;
-    if (is_set(opts & directory_options::include_created_events)) {
-        ev |= change_event::created|change_event::renamed;
-    }
-    if (is_set(opts & directory_options::include_modified_events)) {
-        ev |= change_event::content_modified;
-    }
-    return ev;
-}
-
-inline bool required(fs::change_event ev) {
-    return is_set(ev & fs::change_event::rescan_required);
-}
-
-using notification_ptr = fs::change_notification*;
-
-static_assert(std::is_pointer<fs::change_iterator_config::filter_type>::value, "Broken assumption, values should probably be changed to refs in call().");
-
-notification_ptr call(fs::change_iterator_config::filter_type f, notification_ptr p) {
-    return p && f && !f(*p) ? nullptr : p;
-}
-
-notification_ptr call(const fs::change_iterator_config::filters_type& fl, notification_ptr p) {
-    if (p && !required(p->event())) {
-        for (auto f : fl) {
-            if (!call(f, p)) {
-                return nullptr;
-            }
-        }
-    }
-    return p;
-}
-
-using fsiterator_state = fs::ifilesystem::iterator_state;
-using fsiterator_cache = fs::ifilesystem::cache_info;
-
-struct test_state; // for testing
-
-class state : public fsiterator_state {
-    using lock_type = std::mutex;
-    using lock_guard = std::lock_guard<lock_type>;
-    using callback_type = decltype(fs::change_iterator_config::callback);
-    
-    fs::change_registration m_reg;
-    lock_type mutable m_lock;
-    std::unordered_set<prosoft::stable_hash_wrapper<fs::path>> m_entries;
-    std::atomic_bool m_done{false}; // no more events will be received
-    callback_type m_callback;
-    fs::change_iterator_config::filters_type m_filters;
-    
-    static notification_ptr filter(fs::change_notification& n, fs::change_event ev) {
-        return is_set(n.event() & ev) ? &n : nullptr;
-    }
-    static void filter(fs::change_notification&&, fs::change_event) = delete;
-    
-    notification_ptr PS_ALWAYS_INLINE filter(notification_ptr p) {
-        return call(m_filters, p);
-    }
-    
-    void add(notification_ptr);
-    
-    void abort() noexcept;
-    
-    void notify() noexcept;
-    
-    // testing
-    friend test_state;
-    state() = default;
-public:
-    using fsiterator_state::fsiterator_state;
-    
-    state(const fs::path&, fs::directory_options, fs::change_iterator_config&&, fs::error_code&);
-    virtual ~state();
-    
-    bool done() const {
-        return m_done;
-    }
-    
-    const fs::change_registration& registration() const noexcept {
-        return m_reg;
-    }
-    
-    extraction_type extract();
-    
-    virtual fs::path next(fsiterator_cache&, prosoft::system::error_code&) override;
-    virtual bool at_end() const override;
-    
-    std::string serialize() const {
-        return m_reg.serialize();
-    }
-};
+namespace prosoft {
+namespace filesystem {
+inline namespace v1 {
 
 state::state(const fs::path& p, fs::directory_options opts, fs::change_iterator_config&& c, fs::error_code& ec)
     : fsiterator_state(p, opts, ec)
@@ -225,12 +122,6 @@ bool state::at_end() const {
 }
 
 constexpr auto make_opts_required = fs::directory_options::include_created_events|fs::directory_options::include_modified_events;
-
-} // anon
-
-namespace prosoft {
-namespace filesystem {
-inline namespace v1 {
 
 ifilesystem::iterator_state_ptr
 ifilesystem::make_iterator_state(const path& p, directory_options opts, change_iterator_traits::configuration_type&& c, error_code& ec) {
